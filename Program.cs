5bad944cce6a51ed42688f68867fed9b029bb388
MazenOsama10 <121695429+MazenOsama10@users.noreply.github.com>
2022-12-30 23:41:47 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Services;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the first number");
+            int firstnum = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the end number");
+            int endnum = int.Parse(Console.ReadLine());
+            Boolean f=true;
+            for (int i = firstnum; i <= endnum; i++)
+            {
+                for (int j = 2; j < i; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        f = false;
+                        break;
+                    }
+                    else
+                    {
+                        f = true;
+                        continue;
+                    }
+                }
+                if (f == true)
+                    Console.WriteLine(i);
+            }
+             
+        }
+    }
+}
